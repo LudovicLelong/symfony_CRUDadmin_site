10a846c040593ddba179c3328aca68e081700f0f
LudovicLelong <ludoviclelong10@gmail.com>
2022-08-29 16:46:42 +0200

    [Add] fichier de départ next and first route and twig configuration with homepage : '/'.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-
-class HomeController extends AbstractController {
-
-
+class HomeController extends AbstractController
+{
     #[Route('/', name: 'home', methods: 'GET')]
-    function index(): Response
+    public function index(): Response
     {
-        return $this->render('home/home_index.html.twig');
+        return $this->render ('home/home_index.html.twig');
     }
-
-
 }
